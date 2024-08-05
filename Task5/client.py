8d5fb21f93741f07db9167c415ba60fe5bd07590
HOME <kraynoff.i@yandex.ru>
2024-08-06 01:36:42 +0300

    Add recognizer and message control to server

--- Task5/client.py
+++ Task5/client.py
@@ -1,35 +1,44 @@
 import socket
 from pathlib import Path
-from recogniser import Signal
+from recognizer import Signal
 import numpy as np
+import server
+import time
+from tqdm import tqdm
 
 HOST = "127.0.0.1"
 PORT = 65432
-DISCONNECT_MESSAGE = np.array([999], dtype=np.float32)
 
-s = socket.socket()
+server_socket = socket.socket()
 
 
 def split(signal: Signal, chuncks_n: int):
-    """Yield successive n-sized chunks from lst"""
+    """Yield successive n-sized chunks from l-st"""
 
     for i in range(0, len(signal.wave), chuncks_n):
         yield signal.wave[i : i + chuncks_n]
 
 
-audio = Signal.load(Path('Task5/Data/test1.wav'))
-
+audio = Signal.load(Path(r'Task5/Data/test1.wav'))
 data = input('Enter:')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.connect((HOST, PORT))
     print(f'Connected to {HOST}:{PORT}')
-    for chunk in split(audio, 1000):
-        s.sendall(np.array(chunk, dtype=np.float32).tobytes())
-        s.recv(2048).decode()
 
-    s.send(DISCONNECT_MESSAGE.tobytes())
+    server.send_msg(server_socket, str(audio.sample_rate).encode())
 
-    msg = s.recv(1024)
-    print('Result: ', msg.decode())
-    s.close()
+    _len = len(audio.wave)
+    iterable = tqdm(split(audio, 1000), total=_len // 1000 + np.sign(_len % 1000))
+    for chunk in iterable:
+        packet = np.array(chunk, dtype=np.float32).tobytes()
+        server.send_msg(server_socket, packet)
+        time.sleep(0.2)
+
+    server.send_msg(server_socket, server.END_MESSAGE.encode())
+
+    msg = server.recv_msg(server_socket)
+    if msg:
+        print('Result: ', msg.decode())
+
+    server_socket.close()
--- Task5/server.py
+++ Task5/server.py
@@ -1,18 +1,102 @@
 import multiprocessing
 import threading
 import socket
-import time
+import struct
+
 import vosk
 from pathlib import Path
-import recogniser
+
+
+import recognizer as recognizer
 import numpy as np
 
 
-def pretty_address(address: tuple[str, str]):
+def get_pretty_address(address: tuple[str, str]):
+    """Get adress pretty string"""
+
     return f'{address[0]}:{address[1]}'
 
 
+END_MESSAGE = 'END'
+RECIEVE_MESSAGE = '_CHUNK_RECIEVED_'
+
+
+def send_msg(socket: socket.socket, message: bytes):
+    """Prefix each message with a 4-byte length (network byte order)"""
+
+    message = struct.pack('>I', len(message)) + message
+    socket.sendall(message)
+
+
+def _recvall(socket: socket.socket, n: int) -> bytearray:
+    """Helper function to recv n bytes or return None if EOF is hit"""
+
+    data = bytearray()
+    while len(data) < n:
+        packet = socket.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+
+def recv_msg(socket: socket.socket) -> bytearray:
+    """Read message length and unpack it into an integer"""
+
+    raw_msg_len = _recvall(socket, 4)
+    if not raw_msg_len:
+        return None
+    msglen = struct.unpack('>I', raw_msg_len)[0]
+    return _recvall(socket, msglen)
+
+
 class Server:
+
+    def __init__(self, socket: socket.socket, model) -> None:
+        self.model = model
+        self.socket = socket
+        self.is_stopped = False
+        self._address_pull: dict[str, multiprocessing.Process] = {}
+
+    def accept(self, stop_func: 'function'):
+        """
+        Loop for accept new clients in separate thread, start new processes
+
+        :param stop_func: Event for stop thread
+        """
+        while not stop_func():
+            try:
+                client_socket, address = self.socket.accept()
+            except:
+                break
+            print(f'Connected: {get_pretty_address(address)}')
+
+            process = multiprocessing.Process(
+                target=connect_client,
+                args=(client_socket, address, self.model),
+                daemon=True,
+            )
+            process.start()
+            self._address_pull[get_pretty_address(address)] = process
+        print('Listening stopped')
+
+    def start(self):
+        """Start server"""
+
+        self.socket.bind((HOST, PORT))
+        self.socket.listen()
+
+        print("Start server")
+        self._accept_thread = threading.Thread(
+            target=self.accept,
+            args=(lambda: self.is_stopped,),
+            daemon=True,
+        )
+        self._accept_thread.start()
+
+    def del_client(self, address: tuple[str, str]):
+        del self._address_pull[get_pretty_address(address)]
+
     @staticmethod
     def _command(func=None, get=False, funcs: dict[str, 'function'] = {}):
         """
@@ -24,50 +108,18 @@ class Server:
         :return: Wrapped function
         """
         if not get:
-            funcs.update({func.__name__: func})
+            funcs.update({func.__name__.replace('_', ' '): func})
             return func
         else:
             return funcs
 
-    def __init__(self, socket: socket.socket) -> None:
-        self.socket = socket
-        self.is_stopped = False
-        self._address_pull: dict[str, multiprocessing.Process] = {}
-
-    def accept_client(self, stop_func: 'function'):
-        """
-        Loop for accept new clients in separate thread, start new processes
-
-        :param stop_func: Event for stop thread
-        """
-        while not stop_func():
-            try:
-                client_socket, address = self.socket.accept()
-            except:
-                break
-            print(f'Connected: {pretty_address(address)}')
-
-            process = multiprocessing.Process(
-                target=receive,
-                args=(client_socket, address),
-                # daemon=True,
-            )
-            process.start()
-            self._address_pull[pretty_address(address)] = process
-
-    def start(self):
-        """Start server"""
-
-        self.socket.bind((HOST, PORT))
-        self.socket.listen()
-
-        print("Start server")
-        self._accept_thread = threading.Thread(
-            target=self.accept_client,
-            args=(lambda: self.is_stopped,),
-            daemon=True,
-        )
-        self._accept_thread.start()
+    def execute_command(self, command: str):
+        parsed_command = command.strip()
+        commands = self._command(get=True)
+        if parsed_command in commands.keys():
+            return commands.get(parsed_command)(self)
+        else:
+            print('Unknown command')
 
     @_command
     def stop(self):
@@ -89,17 +141,6 @@ class Server:
         self.stop()
         return -1
 
-    def del_client(self, address: tuple[str, str]):
-        del self._address_pull[pretty_address(address)]
-
-    def execute(self, command: str):
-        parsed_command = command.strip().replace(' ', '_')
-        commands = self._command(get=True)
-        if parsed_command in commands.keys():
-            return commands.get(parsed_command)(self)
-        else:
-            print('Unknown command')
-
     @_command
     def help(self):
         """Help"""
@@ -109,53 +150,68 @@ class Server:
             print(f"{command_name} -- {command.__doc__}")
 
     @_command
-    def clients_list(self):
+    def client_list(self):
         """List of active clients"""
 
         self._address_pull = {
-            key: process for key, process in self._address_pull if process.is_alive()
+            key: process for key, process in self._address_pull.items() if process.is_alive()
         }
 
         print('\n'.join(self._address_pull.keys()).strip())
 
 
-def receive(connection: socket.socket, address: tuple[str, str]):
+def connect_client(
+    client_socket: socket.socket,
+    address: tuple[str, str],
+    model,
+):
     """
     Working function connect and recognize
 
     :param connection: Client socket
     :param address: Client address
     """
-    res = []
-    with connection:
-        print(f'Start recognize for {pretty_address(address)}')
+    recieved_audio = []
+    with client_socket:
+        print(f'Open connection : {get_pretty_address(address)}')
+
+        sample_rate = int(recv_msg(client_socket).decode())
+
         while True:
-            data = connection.recv(2048)
-            data = np.frombuffer(data, dtype=np.float32)
-            if np.average(data) > 1:
+            msg = recv_msg(client_socket)
+
+            if not msg or (msg == END_MESSAGE.encode()):
                 break
-            res.append(data)
-            connection.sendall('Chunk received'.encode())
-        time.sleep(1)
+            data = np.frombuffer(msg, dtype=np.float32)
+            recieved_audio.append(data)
 
-        print(f'End recognize for {pretty_address(address)}')
-        connection.sendall(str(res).encode())
+        wave = np.concatenate(recieved_audio, dtype=np.float32)
+
+        signal = recognizer.Signal(wave=wave, sample_rate=sample_rate)
+        # signal.to_wav(Path('Server_save'), 'Ser1')
+        # print('Signal saved')
+        # text = rec.recognize_signal(signal)
+        print(type(model))
+        # print(text)
+
+        print(f'Close connection: {get_pretty_address(address)}')
+        send_msg(client_socket, 'Disconnect'.encode())
 
 
-DISCONNECT_MESSAGE = np.array([999], dtype=np.float32)
 HOST = "127.0.0.1"
 PORT = 65432
 
 if __name__ == '__main__':
-    # MODEL = vosk.Model(Path(r"D:\Works\TechTasks\vosk-model-ru-0.42").as_posix())
-    # MODEL_SAMPLE_RATE = 8000
+    MODEL = vosk.Model(Path(r"D:\WORKS\TechTasks\vosk-model-ru-0.42").as_posix())
+    MODEL_SAMPLE_RATE = 8000
+    rec = recognizer.Recognizer(MODEL, MODEL_SAMPLE_RATE)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server = Server(server_socket)
+        server = Server(server_socket, rec._rec)
         server.start()
 
         while True:
             command = input()
-            callback = server.execute(command)
+            callback = server.execute_command(command)
             if callback == -1:
                 break
